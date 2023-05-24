8835b09ab782a75da4afd2ae9c290a421182106a
shelson <shelsonx@gmail.com>
2023-05-24 10:56:13 -0300

    create upload file but put

--- backup/app.py
+++ backup/app.py
@@ -1,4 +1,4 @@
-from chalice import Chalice
+from chalice import Chalice, Response
 import logging
 import boto3
 from botocore.exceptions import ClientError
@@ -22,14 +22,47 @@ def create_bucket(bucket_name):
     else:
         return response
 
+def upload_file(file_name, bucket, object_name=None):
+    try:
+        if object_name is None:
+            object_name = os.path.basename(file_name)
+        response = s3_client.upload_file(
+            file_name, bucket, object_name)
+    except ClientError:
+        # logger.exception('Could not upload file to S3 bucket.')
+        raise
+    else:
+        return response
+
 app = Chalice(app_name='backup')
 
 
 @app.lambda_function()
 def first_function(event, context):
-    create_bucket('test2')
+    create_bucket('backup')
 
 
 @app.lambda_function()
 def second_function(event, context):
-    return {'hello': 'world2'}
+   return upload_to_s3('test_up.txt')
+
+
+@app.route('/upload/{file_name}', methods=['PUT'],
+           content_types=['application/octet-stream'])
+def upload_to_s3(file_name):
+
+    # get raw body of PUT request
+    body = app.current_request.raw_body
+    print("\n\nbody: ", body)
+
+    # write body to tmp file
+    tmp_file_name = '/tmp/' + 'file_name'
+    with open(tmp_file_name, 'wb') as tmp_file:
+        tmp_file.write(body)
+
+    # upload tmp file to s3 bucket
+    s3_client.upload_file(tmp_file_name, 'backup', file_name)
+
+    return Response(body='upload successful: {}'.format(file_name),
+                    status_code=200,
+                    headers={'Content-Type': 'text/plain'})
